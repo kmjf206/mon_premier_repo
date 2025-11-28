3a95c024f451f8376dbf8b25eaa291126b1f88d5
kalamo joel <kalamojoel79@gmaol.com>
2025-11-28 17:02:18 +0300

    on a cree deux dossiers pour le code en python et javascript

---
+++ code_js/text_1.js
@@ -0,0 +1,12 @@
+// javascript-accolades
+let age = 12;
+
+if(age < 18){
+    console.log("mineur");
+    acces = false;
+} else if (age <65){console.log("adulte");
+    acces = true;
+}else{
+    console.log("senior");
+    acces = true
+}
---
+++ code_python/text_1.py
@@ -0,0 +1,10 @@
+#python -indantation signaficative
+if age <18:
+    print("mineur")
+    acces=false
+    elif age < 65:
+        print("aldulte")
+        acces = true
+        else: 
+            print("senior")
+            acces = true
